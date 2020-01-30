12ebf6dc1a4e126676e08a54759ceb1eb85b6428
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-01-30 22:02:44 +0100

    Solves a problem with the gitignore file

---
+++ data/buttonText.js
@@ -0,0 +1,15 @@
+const buttonText = [
+  "Ugh.",
+  "🤦🏻‍♂️",
+  "☹️",
+  "omg dad.",
+  "you are the worst",
+  "seriously",
+  "stop it.",
+  "please stop",
+  "that was the worst one",
+  "ufff",
+  "really?"
+];
+
+export default buttonText;
